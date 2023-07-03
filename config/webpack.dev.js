eaef185ae9b858850c8241e527e54de0b9c6d8c9
mewcoder <1010953107@qq.com>
2023-07-03 22:23:22 +0800

    feat: update project

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -24,7 +24,7 @@ module.exports = () => {
       historyApiFallback: {
         index: paths.getPublicPath()
       },
-      // open: [paths.getPublicPath()],
+      open: [paths.getPublicPath()],
       proxy
     },
     optimization: {
--- src/views/about.vue
+++ src/views/about.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <div>{{ obj }}</div>
+    <pre>{{ obj }}</pre>
   </div>
 </template>
 
